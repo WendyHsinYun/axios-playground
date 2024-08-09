be39f907ebfedce0ae1ccdc9394f8c9d0eb93169
HsinYun <hsinyun.chang@ionetworks.co>
2024-08-09 15:17:15 +0800

    first commit

---
+++ src/components/AnimalFacts.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <h3>Cat Facts</h3>
+    </div>
+    <div class="col-md-12">
+      <ul class="list-group">
+        <li v-for="(fact, index) in catFacts" :key="index" class="list-group-item">{{index + 1}}. {{fact.text}}</li>
+        <!-- <li v-for="(fact, index) in catFacts" :key="index" class="list-group-item">{{index + 1}}. {{fact}}</li> -->
+      </ul>
+    </div>
+    <div class="row mt-3">
+      <div class="col-md-12 text-center">
+        <button @click="loadMoreFacts" class="btn btn-md btn-primary">{{ isFetchingFacts ? '...' : 'Load more' }}</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineOptions, Ref } from 'vue'; 
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+defineOptions({
+  name: 'AnimalFacts'
+});
+
+interface AnimalFacts {
+  text: string
+}
+
+const catFacts = ref<AnimalFacts[]>([])
+const isFetchingFacts: Ref<Boolean> = ref(false)
+
+// load more cat facts
+const loadMoreFacts = async() => {
+  isFetchingFacts.value = true
+
+  const catFactsResponse = await axios.get<AnimalFacts[]>('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=5')
+  catFacts.value.push(...(catFactsResponse.data || [])) // append new facts to existing facts array, if no new facts, append empty array
+
+  isFetchingFacts.value = false
+}
+
+// fetch initial cat facts
+const fetchInitialCatFacts = async() => {
+  const catFactsResponse = await axios.get<AnimalFacts[]>('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=5')
+
+  catFacts.value = catFactsResponse.data
+}
+
+const testAxios = async() => {
+  await axios.get<AnimalFacts[]>('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=5')
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error))
+}
+
+onMounted(async () => {
+  await fetchInitialCatFacts()
+  await testAxios()
+})
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <main>
+    <AnimalFacts />
+  </main>
+</template>
+
+<script setup lang="ts">
+import AnimalFacts from '../components/AnimalFacts.vue'
+</script>
+
